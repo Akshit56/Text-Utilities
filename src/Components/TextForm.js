f5a823eb55554c9cfd71d277a43b1a1b8399efdf
Akshit Gupta <akshit0689@gmail.com>
2024-08-11 15:58:37 +0530

    added preview option

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -28,11 +28,13 @@ export default function TextForm(props) {
         <h1>{props.heading}</h1>
         <textarea className="form-control" id="exampleFormControlTextarea1" rows="8" value = {text} onChange={handleOnChange}></textarea>
       </div>
-      <button className="bt btn-submit" onClick={handleUpClick}>Convert to Uppercase</button>
+      <button className="bt btn-submit mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
       <button className="bt btn-submit" onClick={handleLoClick}>Convert to Lowercase</button>
     <h1> Summary</h1>
     <p>{text.split(" ").length} Words {text.length} Characters</p>
     <p>{text.split(" ").length *0.008} Minutes read</p>
+    <h2>Preview</h2>
+    <p>{text}</p>
     </div>
   )
 }
