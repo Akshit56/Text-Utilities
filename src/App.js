bae6518bf99d7477542daa8f2fe1250295722c6c
Akshit Gupta <akshit0689@gmail.com>
2024-08-11 15:11:25 +0530

    Creating a form where the user can input the text to be edited

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Navbar from './Components/Navbar';
 import './App.css';
+import TextForm from './Components/TextForm';
 
 function App() {
   return (
-    <Navbar title = 'Navbar2' about = "About Us" />
-    // <Navbar />
+    <>
+      <Navbar title = 'Navbar2' about = "About Us" />
+    {/* // <Navbar /> */}
+    <TextForm />
+    </>
+    
   );
 }
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -15,10 +15,10 @@ export default function Navbar(props) {
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">{props.about}</a>
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/">Link</a>
+          <a className="nav-link" href="/">{props.about}</a>
         </li>
         <li class="nav-item dropdown">
           <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
