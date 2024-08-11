b7e0fc4bc72e81543760af6bfdd84a6a97af4117
Akshit Gupta <akshit0689@gmail.com>
2024-08-11 15:42:06 +0530

    Added functionality to capitalize the text entered by the user in the text area

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function TextForm(props) {
+
+  const handleUpClick = () => {
+    console.log('Uppercase button was clicked')
+    let newText = text.toUpperCase();
+    setText(newText)
+  }
+
+  const handleOnChange = (event) => {
+    console.log('On Change')
+    setText(event.target.value)
+  }
+
+  const [text, setText] = useState('Enter Text');
+  // setText = ("Enter text here")
   return (
+    
     <div>
       <div className="mb-3">
         <h1>{props.heading}</h1>
-        <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-        <textarea className="form-control" id="exampleFormControlTextarea1" rows="8"></textarea>
+        <textarea className="form-control" id="exampleFormControlTextarea1" rows="8" value = {text} onChange={handleOnChange}></textarea>
       </div>
-      <button className="bt btn-submit">Convert to Uppercase</button>
+      <button className="bt btn-submit" onClick={handleUpClick}>Convert to Uppercase</button>
     </div>
   )
 }
