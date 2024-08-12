1c25ce34877f85312910bf14e630254c907f8ef0
Akshit Gupta <akshit0689@gmail.com>
2024-08-12 14:38:20 +0530

    Added dark mode functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import Navbar from './Components/Navbar';
 import './App.css';
-import TextForm from './Components/TextForm';
+// import TextForm from './Components/TextForm';
+import About from './Components/About'
 
 function App() {
   return (
     <>
       <Navbar title = 'TextUtils' about = "About Us" />
       <div className="container my-3"> {/* Margin in y axis of value 3*/ }
-        <TextForm heading = "Enter text below"/>
+        {/* <TextForm heading = "Enter text below"/> */}
+      <About />
       </div>
     </>
-    
   );
 }
 
